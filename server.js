a36ff6bca9db35bf24e1784464f66421acdb2d1a
manish-nagar27 <manish@signwalla.com>
2024-09-16 13:45:01 +0530

    add env

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
 const app = express();
+
+require("dotenv").config();
 
 app.get("/api/get", (req, res) => {
   res.send({ message: "Hello, World!" });
 });
-
-app.listen(8000, () => {
+app.get("/api/get-details", (req, res) => {
+  res.send({ Name: process.env.NAME });
+});
+app.listen(process.env.PORT, () => {
   console.log("App is running  on port 8000");
 });
